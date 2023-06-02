d8f0cbcbe90345e31525ca112ecd90c1a466aedf
ashiq <ashiqachu1@gmail.com>
2023-06-02 02:00:21 +0100

    test commit

--- cmds/main.sh
+++ cmds/main.sh
@@ -2,9 +2,6 @@
 for input in ./input/*.json; do	
     filename=$(basename $input '.json')
     output="./output/$filename.html"
-	pwd
 	touch $output
-	ls
-	echo "process"
 	./cmds/transform.sh $input $output
 done
